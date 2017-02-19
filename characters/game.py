b13660a8d5a7c3bbf1e0123135fbc0972d7e5264
Kaspars Primaks <kaspars.primaks@gmail.com>
2017-02-19 23:04:16 +0000

    use invader class and move invader

--- characters/game.py
+++ characters/game.py
@@ -1,5 +1,5 @@
 import pygame
-import invader
+from Invader import Invader
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -8,31 +8,43 @@ pygame.init()
 
 screen = pygame.display.set_mode([500, 500])
 
-frames = invader.read_frames('invader')
+l = 10
+invader = Invader('ufo', l, BLACK, screen)
+pos = { 'x': 0, 'y': 0 }
 
-def draw_frame(index, size):
-    for c in frames[index]:
-        rect = pygame.Rect(c[0] * 10, c[1] * 10, c[2] * size, size)
-        pygame.draw.rect(screen, BLACK, rect, 0)
-
-i = 0
 done = False
+_x = 0
+_y = -1
 while not done:
     pygame.time.wait(250)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and _y < 0:
+                _x = pos['x'] + 70
+                _y = 190
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP]:
+        pos['y'] = pos['y'] - l
+    if keys[pygame.K_DOWN]:
+        pos['y'] = pos['y'] + l
+    if keys[pygame.K_LEFT]:
+        pos['x'] = pos['x'] - l
+    if keys[pygame.K_RIGHT]:
+        pos['x'] = pos['x'] + l
 
     screen.fill(WHITE)
 
-    if i >= len(frames):
-        i = 0
+    invader.draw_frame(pos['x'], pos['y'] + 200)
+    invader.next_frame()
 
-    draw_frame(i, 10)
-
-    i = i + 1
+    if _y > -1:
+        beam = pygame.Rect(_x, _y, l, 2 * l)
+        pygame.draw.rect(screen, BLACK, beam, 0)
+        _y = _y - l
 
     pygame.display.flip()
